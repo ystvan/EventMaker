fa8bb5a3e73a4b99e061950529895503c83ee622
ystvan <istv0050@edu.easj.dk>
2016-01-28 21:56:16 +0100

    new design

--- Eventmaker/App.xaml.cs
+++ Eventmaker/App.xaml.cs
@@ -76,7 +76,7 @@ namespace Eventmaker
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(EventPage), e.Arguments);
+                rootFrame.Navigate(typeof(CreateEventPage), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
