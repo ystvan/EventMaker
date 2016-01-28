aa7b528a7f7e5bcc2b4974e93ee874fbf41105fc
ystvan <istv0050@edu.easj.dk>
2016-01-28 23:03:18 +0100

    new class

---
+++ Eventmaker/Handler/EventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eventmaker.ViewModel;
+
+namespace Eventmaker.Handler
+{
+    class EventHandler
+    {
+        public EventViewModel EventViewModel { get; set; }
+
+        public EventHandler()
+        {
+           EventViewModel = new EventViewModel();
+        }
+
+        // Not sure about the method below step L) in the SWCLectureDrive
+        public void TriggerCreateEventMethod()
+        {
+            EventViewModel.CreateEvent();
+        }
+    }
+}
