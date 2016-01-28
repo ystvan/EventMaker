2657785fd1f57e3da0c055d5c97895650ef50aa2
ystvan <istv0050@edu.easj.dk>
2016-01-29 00:33:46 +0100

    json

---
+++ Eventmaker/Persistency/PersistencyService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Eventmaker.Model;
+
+namespace Eventmaker.Persistency
+{
+    class PersistencyService
+    {
+        public static async void SaveEventsAsJsonAsync(ObservableCollection<Event> Events)
+        {
+            StorageFolder 
+        }
+
+        public static async Task<List<Event>> LoadEventsFromJsonAsync()
+        {
+            
+        }
+
+        public static async void SerializeEventsFileAsync(string eventsString, string fileName)
+        {
+            
+        }
+
+        public static async Task<string> DeSerializeEventsFileAsync(String fileName)
+        {
+            
+        }
+    }
+}
